0db79453d7555d754542bcac4dd9762a55d57d34
Hermes Santos <hermessantos995@gmail.com>
2023-01-27 17:06:04 -0300

    refactoring the code

--- index.js
+++ index.js
@@ -1,50 +1,57 @@
 import nodeNotifier from 'node-notifier';
-let multiplier = 10
+let multiplier = 40
 let counter = 1
 let focusTime = false
 let index = 1
-let progressBar = ['[','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-',']']
+let progressBar = ['[', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', ']']
+let focusSeconds = 1200
+let breakSeconds = 300
 
 setInterval(() => {
-  if(counter === 300000 && focusTime === false) {
+  if (counter === focusSeconds && focusTime === false) { //seconds
     notifier()
     counter = 0
     focusTime = true
-    console.log({"focusTime": focusTime})
   }
-  if(counter === 1200000 && focusTime === true) {
+  if (counter === breakSeconds && focusTime === true) { //seconds
     counter = 0
     focusTime = false
     notifier2()
   }
+
   console.clear()
-  if(counter>=60){
+  
+  if (counter >= 60) {
     console.clear()
-    let min = Math.floor(counter/60)
-    let sec = counter - min*60 
+    let min = Math.floor(counter / 60)
+    let sec = counter - min * 60
     console.log(`${min} min e ${sec} seconds`)
   } else {
     console.log(`${counter} seconds`)
   }
+
   !focusTime ? console.log("Focus time") : console.log("Breaking time")
   counter++
+
   let joinedArray = progressBar.join("")
   console.log(joinedArray)
-  if(counter===multiplier){
-    progressBar[index] =  "="
-    index++
-    multiplier=multiplier+10
-  }
-}, 1000)
 
-function notifier2 () {
+  if (counter === multiplier) {
+    progressBar[index] = "="
+    index++
+    multiplier = 2 * multiplier
+  }
+}, 1000) //ms
+
+//notifiers
+function notifier2() {
   nodeNotifier.notify({
     title: 'Pomodoro',
     message: 'Focus time',
     sound: true
   });
 }
-function notifier () {
+function notifier() {
   nodeNotifier.notify({
     title: 'Pomodoro',
     message: 'Breaking time',
